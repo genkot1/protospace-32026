598524496053409d380debd9c53313b8751148ed
genkot1 <gentaro.kotaka@gmail.com>
2020-11-17 00:04:38 +0900

    削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -35,6 +35,11 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+    Prototype.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
